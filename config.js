b7329998cbc080819eadd80c128776ce0afcfaa8
Plamena Petkova <pl.petkova@gmail.com>
2024-05-13 18:28:52 +0300

    Fix the way that we use env variables

--- config.js
+++ config.js
@@ -1,30 +1,33 @@
 // Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
 import firebase from "firebase/compat/app";
 import 'firebase/compat/storage';
-import { initializeApp } from "firebase/compat/app";
-import { getStorage } from "firebase/compat/storage";
-import Constants from 'expo-constants';
+//import { initializeApp } from "firebase/compat/app";
+//import { getStorage } from "firebase/compat/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: Constants.API_KEY,
-  authDomain: Constants.AUTH_DOMAIN,
-  projectId: Constants.PROJECT_ID,
-  storageBucket: Constants.STORAGE_BUCKET,
-  messagingSenderId: Constants.MESSAGING_SENDER_ID,
-  appId: Constants.APP_ID,
-  measurementId: Constants.MEASUREMENT_ID,
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
 };
 
-console.log('Storage bucket', Constants.API_KEY)
+console.log('Storage bucket', process.env.EXPO_PUBLIC_API_KEY)
 
  //Initialize Firebase
-//if(firebase.apps.length) {
+
     firebase.initializeApp(firebaseConfig);
-//}
+
+//const app = initializeApp(firebaseConfig);
+
 
 
 export {firebase};
